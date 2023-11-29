d566f3305d6d7cd2408ab6068b7f176c698297fd
SilverRainbovv <vadimdidenko156@gmail.com>
2023-11-29 12:59:57 +0200

    app init

---
+++ src/main/java/com/didenko/starcruises/StarCruisesApplication.java
@@ -0,0 +1,13 @@
+package com.didenko.starcruises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarCruisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarCruisesApplication.class, args);
+    }
+
+}
